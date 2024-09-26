317e7734c3c0eb166e23d79f8bb7d91ca08178d3
Cindy Jiang <ziyijian@andrew.cmu.edu>
2024-09-26 15:15:14 -0400

    first commit

---
+++ PriceCheck/Views/LocationView.swift
@@ -0,0 +1,8 @@
+//
+//  LocationView.swift
+//  PriceCheck
+//
+//  Created by Cindy Jiang on 2024/9/26.
+//
+
+import Foundation
---
+++ PriceCheck/Views/ScanView.swift
@@ -0,0 +1,8 @@
+//
+//  ScanView.swift
+//  PriceCheck
+//
+//  Created by Cindy Jiang on 2024/9/26.
+//
+
+import Foundation
